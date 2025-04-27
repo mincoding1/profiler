55e3a9bcd95ff4eee228c69f63065e8158f6655a
mincoding <74457149+mincoding1@users.noreply.github.com>
2025-04-27 22:09:42 +0900

    Update profiler_temp.cpp

--- cpp/profiler_temp.cpp
+++ cpp/profiler_temp.cpp
@@ -1,31 +1,28 @@
 #include <iostream>
 #include <string>
 #include <windows.h>
-using namespace std;
 
-//============================================================================================================
-//[경로 Check 필요!] 
-//============================================================================================================
-//빌드 프로그램 파일 경로 (MSBuild.exe)
-const string msbuild_path = R"(C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe)";
+//=============================================================================================================================
+//[!!] 빌드 프로그램 파일 경로 (MSBuild.exe), 필요시 본인에게 맞게 수정 필요
+const std::string msbuild_path = R"(C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe)";
 
-//프로젝트 파일 경로 (target.vcxproj)
-const string pjt_path = R"(C:\Users\user\source\repos\profiler\target\target.vcxproj)";
+//[!!] 솔루션 경로를 본인에게 맞게 수정 필요.
+const std::string solution_path = R"(C:\Users\minco\source\repos\Solution98\)";
+//=============================================================================================================================
 
-//빌드 후 실행해야 할 파일 경로 (target.exe)
-const string exe_path = R"(C:\Users\user\source\repos\profiler\target\Release\target.exe)";
-//============================================================================================================
+const std::string pjt_path = solution_path + R"(Target\Target.vcxproj)"; // 빌드 할 프로젝트 파일 경로 (target.vcxproj)
+const std::string exe_path = solution_path + R"(Target\Release\Target.exe)"; // 빌드 후 실행해야 할 파일 경로 (target.exe)
 
 void build() {
     //Release Mode로 Build
-    const string option = R"(/p:Configuration=Release)";
+    const std::string option = R"(/p:Configuration=Release)";
 
-    string cmd = string{ "\"" } + msbuild_path + string{ "\" " } + pjt_path + string{ " " } + option;
+    std::string cmd = std::string{ "\"" } + msbuild_path + std::string{ "\" " } + pjt_path + std::string{ " " } + option;
     system(cmd.c_str());
 }
 
 void run() {
-    string cmd = string{ "\"" } + exe_path + string{ "\"" };
+    std::string cmd = std::string{ "\"" } + exe_path + std::string{ "\"" };
     system(cmd.c_str());
 }
 
