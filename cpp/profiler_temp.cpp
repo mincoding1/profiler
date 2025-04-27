1ab701fdacf03c0c064beaa5702bc1507234ce47
mincoding <74457149+mincoding1@users.noreply.github.com>
2025-04-27 22:16:09 +0900

    Update profiler_temp.cpp

--- cpp/profiler_temp.cpp
+++ cpp/profiler_temp.cpp
@@ -1,14 +1,16 @@
-#include <iostream>
-#include <string>
-#include <windows.h>
-
 //=============================================================================================================================
-//[!!] 빌드 프로그램 파일 경로 (MSBuild.exe), 필요시 본인에게 맞게 수정 필요
+#include <string>
+
+//빌드 프로그램 파일 경로 (MSBuild.exe)
 const std::string msbuild_path = R"(C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe)";
 
-//[!!] 솔루션 경로를 본인에게 맞게 수정 필요.
+//솔루션 경로 (필요시 본인에게 맞게 경로 수정 필요)
 const std::string solution_path = R"(C:\Users\minco\source\repos\Solution98\)";
 //=============================================================================================================================
+
+
+#include <iostream>
+#include <windows.h>
 
 const std::string pjt_path = solution_path + R"(Target\Target.vcxproj)"; // 빌드 할 프로젝트 파일 경로 (target.vcxproj)
 const std::string exe_path = solution_path + R"(Target\Release\Target.exe)"; // 빌드 후 실행해야 할 파일 경로 (target.exe)
